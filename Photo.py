26d4b66fcf5009452b56f41bd5ee3b50ee5281f8
k5cui <k5cui@uwaterloo.ca>
2020-04-28 21:01:31 -0400

    we can make a new pic hehehe

--- Photo.py
+++ Photo.py
@@ -1,34 +1,44 @@
 from PIL import Image
 
-im = Image.open("assets/carolyn.png")
+finalX, finalY = 520, 520
+width, height = 40, 40
+sectionWidth, sectionHeight = int(finalX / width), int(finalY / height)
 
-im = im.resize((500, 500))
+def avgColour(imgRegion):
+    r, g, b = [0, 0, 0]
+    for i in range(0, sectionWidth):
+        for j in range(0, sectionHeight):
+            tempr, tempg, tempb = imgRegion.getpixel((i, j))
+            # print(tempr,tempg,tempb)
+            r = r + tempr
+            g = g + tempg
+            b = b + tempb
+            # r,g,b = r,g,b + tempr,tempg,tempb
+    # r,g,b = r,g,b/2500
+    r = int(r / (sectionWidth * sectionHeight))
+    g = int(g / (sectionWidth * sectionHeight))
+    b = int(b / (sectionWidth * sectionHeight))
+    return r, g, b
+    # r, g, b = imgregion.getpixel((1,1))
+    # print(r, g, b)
 
-colourarr = [[]]
+im = Image.open("assets/weeb.jpg")
 
-def avgColour(imgregion):
-    r,g,b = [0,0,0]
-    for i in range(0, 49):
-        for i in range(0,49):
-            tempr,tempg,tempb = imgregion.getpixel((i,j))
-            #print(tempr,tempg,tempb)
-            r=r+tempr
-            g=g+tempg
-            b=b+tempb
-            #r,g,b = r,g,b + tempr,tempg,tempb
-    #r,g,b = r,g,b/2500
-    r=r/2500
-    g=g/2500
-    b=b/2500
-    return r,g,b
-    #r, g, b = imgregion.getpixel((1,1))
-    #print(r, g, b)
+im = im.resize((finalX, finalY))
 
-for i in range(0, 9):
-    for j in range (0, 9):
-        box = (50 * i, 50 * j, 50 * (i + 1), 50 * (j + 1));
+colourArr = [[(0, 0, 0) for x in range(width)] for y in range(height)]
+
+finalImage = Image.new('RGB', (finalX, finalY))
+
+for i in range(0, width):
+    for j in range(0, height):
+        box = (sectionWidth * i, sectionHeight * j, sectionWidth * (i + 1), sectionHeight * (j + 1))
         subsection = im.crop(box)
         sectionRGB = subsection.convert('RGB')
-        avgColour(sectionRGB)
-        #colourarr[]
+        colourArr[i][j] = avgColour(sectionRGB)
+        tempImage = Image.new('RGB', (sectionWidth, sectionHeight), colourArr[i][j])
+        finalImage.paste(tempImage, (i * sectionWidth, j * sectionHeight))
+
+finalImage.save('assets/result.jpg')
+
 
